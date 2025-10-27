f01e80081039e433a47b016c475884179371af20
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-10-27 15:41:30 +0530

    feat:initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin:'http//localhost:5173',
+  credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT =process.env.PORT || 3000;
+ 
+app.listen(PORT,()=>{
+  console.log(`server is running at port ${PORT}`);
+})
