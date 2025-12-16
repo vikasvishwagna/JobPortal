c6a56450979fe801812950871632ebe1e0c19053
vikasvishwagna <vishwagnavikassai@gmail.com>
2025-12-17 00:54:06 +0530

    Fix: added deployment link to cors origin

--- Frountend/src/utils/constants.js
+++ Frountend/src/utils/constants.js
@@ -1,7 +1,18 @@
-export const USERS_API_END_POINT = "http://localhost:8000/api/v1/user";
+export const USERS_API_END_POINT = "https://nexthire-o2t4.onrender.com/api/v1/user";
 
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const JOB_API_END_POINT = "https://nexthire-o2t4.onrender.com/api/v1/job";
 
-export const APPLICATIONS_API_END_POINT ="http://localhost:8000/api/v1/application";
+export const APPLICATIONS_API_END_POINT ="https://nexthire-o2t4.onrender.com/api/v1/application";
 
-export const COMPANY_API_END_POINT ="http://localhost:8000/api/v1/company";
+export const COMPANY_API_END_POINT ="https://nexthire-o2t4.onrender.com/api/v1/company";
+
+
+
+// export const USERS_API_END_POINT = "http://localhost:8000/api/v1/user";
+
+// export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+
+// export const APPLICATIONS_API_END_POINT ="http://localhost:8000/api/v1/application";
+
+// export const COMPANY_API_END_POINT ="http://localhost:8000/api/v1/company";
+
